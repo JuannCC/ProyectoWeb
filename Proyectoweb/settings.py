b325191c3ab415dcdf2abdd7b894a8054f92d6aa
JuannCC <90645839+JuannCC@users.noreply.github.com>
2023-03-24 10:47:21 +0100

    Update settings.py

--- Proyectoweb/settings.py
+++ Proyectoweb/settings.py
@@ -136,8 +136,8 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER="juancruzcrotta@gmail.com"
-EMAIL_HOST_PASSWORD="roncedo12A"
+EMAIL_HOST_USER=""
+EMAIL_HOST_PASSWORD=""
 
 
 
